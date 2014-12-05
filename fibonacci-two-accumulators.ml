6cc5eb0e4f61af2fbe5e5b68171260ec66812e89
Shivan Kaul Sahib <shivankaul.1993@gmail.com>
2014-12-05 00:29:35 -0500

    Update fibonacci-two-accumulators.ml

--- fibonacci-two-accumulators.ml
+++ fibonacci-two-accumulators.ml
@@ -1,4 +1,5 @@
-(* I just implemented a Fibonacci series calculator using two accumulators. Why did I do this? *)
+(* I just implemented a Fibonacci series calculator using two accumulators, making it completely tail-recursive. *)  
+(* It's quite interesting: this was the most obvious way that struck me. It's quite wonderful what 4 months of functional have done to my brain.*)
 
 let fib n =
   let rec fib' x acc1 acc2 = if (x=n) then acc1 else fib' (x+1) acc2 (acc1+acc2)
