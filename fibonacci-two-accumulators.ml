44c5dd6956c7cc025c913b5c090d01feb23fa192
Shivan Kaul Sahib <shivankaul.1993@gmail.com>
2014-12-05 00:18:32 -0500

    Update fibonacci-two-accumulators.ml

--- fibonacci-two-accumulators.ml
+++ fibonacci-two-accumulators.ml
@@ -1,3 +1,5 @@
+(* I just implemented a Fibonacci series calculator using two accumulators. Why did I do this? *)
+
 let fib n =
   let rec fib' x acc1 acc2 = if (x=n) then acc1 else fib' (x+1) acc2 (acc1+acc2)
   in fib' 0 0 1
